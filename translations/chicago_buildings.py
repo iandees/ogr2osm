f25adc25e0a3bcb9cf8c68d228c75bfe17a705e5
Ian Dees <ian.dees@gmail.com>
2012-05-27 23:17:04 -0500

    Some updates after experimenting with possible data values.

--- translations/chicago_buildings.py
+++ translations/chicago_buildings.py
@@ -54,7 +54,7 @@ def filterTags(attrs):
     tags = {}
 
     tags['building'] = 'yes'
-    tags['cookcounty:building_id'] = attrs['BLDG_ID']
+    tags['chicago:building_id'] = attrs['BLDG_ID']
     
     if len(attrs['ST_NAME1']) > 0:
         prefix = translateName(attrs['PRE_DIR1'])
@@ -63,7 +63,20 @@ def filterTags(attrs):
 
         street_name = attrs['ST_NAME1']
         if street_name:
-            tags['addr:street:name'] = street_name.title()
+            street_name = street_name.title().replace('0Th', '0th') \
+                                             .replace('11Th', '11th') \
+                                             .replace('12Th', '12th') \
+                                             .replace('13Th', '13th') \
+                                             .replace('1St', '1st') \
+                                             .replace('2Nd', '2nd') \
+                                             .replace('3Rd', '3rd') \
+                                             .replace('4Th', '4th') \
+                                             .replace('5Th', '5th') \
+                                             .replace('6Th', '6th') \
+                                             .replace('7Th', '7th') \
+                                             .replace('8Th', '8th') \
+                                             .replace('9Th', '9th') 
+            tags['addr:street:name'] = street_name
 
         street_type = translateName(attrs['ST_TYPE1'])
         if street_type:
@@ -73,10 +86,10 @@ def filterTags(attrs):
         if suffix:
             tags['addr:street:suffix'] = suffix
 
-        name = ("%s %s %s %s" % (prefix, street_name, street_type, suffix)).strip().title()
+        name = ("%s %s %s %s" % (prefix, street_name, street_type, suffix)).strip()
 
         tags['addr:street'] = name
-        
+
     if attrs['F_ADD1'] != '0' and attrs['T_ADD1'] != '0':
         from_addr = attrs['F_ADD1']
         to_addr = attrs['T_ADD1']
@@ -84,7 +97,22 @@ def filterTags(attrs):
         if from_addr == to_addr:
             tags['addr:housenumber'] = from_addr
         elif from_addr < to_addr:
-            tags['addr:from'] = from_addr
-            tags['addr:to'] = to_addr
-                   
+            tags['addr:housenumber'] = "%s-%s" % (from_addr, to_addr)
+
+    if len(attrs['NON_STANDA']) > 0:
+        non_standard = attrs['NON_STANDA']
+        if 'RESIDENTIAL GARAGE' == non_standard:
+            tags['building'] = 'garage'
+        elif 'MONUMENT' == non_standard:
+            tags['historic'] = 'monument'
+            del tags['building']
+        elif 'CTA PLATFORM' == non_standard:
+            tags['railway'] = 'platform'
+
+    if len(attrs['BLDG_NAME1']) > 0:
+        tags['name'] = attrs['BLDG_NAME1'].title()
+
+    if len(attrs['STORIES']) > 0 and int(attrs['STORIES']) > 0:
+        tags['building:levels'] = attrs['STORIES']
+
     return tags
